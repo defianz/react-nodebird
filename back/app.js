402e6126dda929e4b3e488490425b218909069fa
Defian <iautm91@gmail.com>
2020-08-31 17:53:05 +0900

    update for back server

--- back/app.js
+++ back/app.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: ["http://localhost:3060", "nodebird.com"],
+    origin: ["http://localhost:3060", "nodebird.com", "http://15.164.98.68"],
     credentials: true, // 쿠키 전달 옵션 : 전달하려면 true (default : false)
   })
 );
