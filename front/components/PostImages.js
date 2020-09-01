0cd51eb17be2976ad71d881c935ddf96a1f9b6ee
Defian <iautm91@gmail.com>
2020-09-01 10:54:35 +0900

    backUrl for image

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -20,7 +20,7 @@ const PostImages = ({ images }) => {
       <>
         <img
           role="presentation"
-          src={`${backUrl}/${images[0].src}`}
+          src={`${images[0].src}`}
           alt={images[0].src}
           onClick={onZoom}
         />
@@ -34,14 +34,14 @@ const PostImages = ({ images }) => {
         <img
           role="presentation"
           style={{ width: "50%", display: "inline-block" }}
-          src={`${backUrl}/${images[0].src}`}
+          src={`${images[0].src}`}
           alt={images[0].src}
           onClick={onZoom}
         />
         <img
           role="presentation"
           style={{ width: "50%", display: "inline-block" }}
-          src={`${backUrl}/${images[1].src}`}
+          src={`${images[1].src}`}
           alt={images[1].src}
           onClick={onZoom}
         />
@@ -55,7 +55,7 @@ const PostImages = ({ images }) => {
         <img
           role="presentation"
           style={{ width: "50%" }}
-          src={`${backUrl}/${images[0].src}`}
+          src={`${images[0].src}`}
           alt={images[0].src}
           onClick={onZoom}
         />
