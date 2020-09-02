328d1180b2a862fc55f6b32d9a53031d85d47b2e
Defian <iautm91@gmail.com>
2020-09-02 21:36:03 +0900

    update for typo

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -30,12 +30,12 @@ const Profile = () => {
   const [followingsLimit, setFollowingsLimit] = useState(3);
 
   const { data: followersData, error: followerError } = useSWR(
-    `http://${backUrl}/user/followers?limit=${followersLimit}`,
+    `${backUrl}/user/followers?limit=${followersLimit}`,
     fetcher
   );
 
   const { data: followingsData, error: followingError } = useSWR(
-    `http://${backUrl}/user/followings?limit=${followingsLimit}`,
+    `${backUrl}/user/followings?limit=${followingsLimit}`,
     fetcher
   );
 
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,31 +1,30 @@
-const { createWrapper } = require('next-redux-wrapper');
-import { applyMiddleware, createStore, compose } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from '../sagas';
+const { createWrapper } = require("next-redux-wrapper");
+import { applyMiddleware, createStore, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas";
 
-import reducer from '../reducers';
-
+import reducer from "../reducers";
 
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
-    console.log(action);
-    return next(action);
-}
-
-const configureStore = () => {
-    const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware, loggerMiddleware];
-    const enhancer = process.env.NODE_ENV === 'production'
-        ? compose(applyMiddleware(...middlewares))
-        : composeWithDevTools(applyMiddleware(...middlewares))
-    const store = createStore(reducer, enhancer);
-    store.sagaTask = sagaMiddleware.run(rootSaga);
-    return store;
+  console.log(action);
+  return next(action);
 };
 
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+};
 
 const wrapper = createWrapper(configureStore, {
-    debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development",
 });
 
 export default wrapper;
