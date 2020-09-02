d79d57cc327277de08a8c96fae44c7f1de6a0e5d
Defian <iautm91@gmail.com>
2020-09-02 21:45:44 +0900

    update for login

--- back/passport/index.js
+++ back/passport/index.js
@@ -4,12 +4,14 @@ const { User } = require("../models");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
+    console.log(user);
     done(null, user.id);
   });
 
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findOne({ where: { id } });
+      console.log(user);
       done(null, user);
     } catch (error) {
       console.error(error);
